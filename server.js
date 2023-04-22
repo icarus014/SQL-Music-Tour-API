120ee82d7f89b69e07a9bd0a006aba8df46a8912
Darian Brown <browndarian011@gmail.com>
2023-04-22 12:54:45 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
-
+const bandsController = require('./controllers/bands_controller')
+const eventsController = require('./controllers/events_controller')
+const stagesController = require('./controllers/stages_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Hello you should not be here'
+        message: 'Hello. You should not be here.'
     })
 })
 
-// CONTROLLERS 
-const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
-
-
-
+app.use('/events', eventsController)
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
