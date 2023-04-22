c1299c4d45d85702ffa82b7500e72f7685c95d26
Darian Brown <browndarian011@gmail.com>
2023-04-22 10:16:24 -0400

    add associations

--- models/event.js
+++ models/event.js
@@ -9,8 +9,21 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Stage, StageEvent, MeetGreet, SetTime}) {
       // define association here
+      Event.belongsToMany(Stage, {
+        through: StageEvent,
+        foreignKey: 'event_id',
+        as: 'stages'
+      })
+      Event.hasMany(MeetGreet, {
+        foreignKey:'event_id',
+        as: 'meet_greets'
+      })
+      Event.hasMany(SetTime, {
+        foreignKey:'event_id',
+        as: 'set_times'
+      })
     }
   }
   Event.init({
--- models/stage.js
+++ models/stage.js
@@ -7,8 +7,18 @@ module.exports = (sequelize, DataTypes) => {
 		 * This method is not a part of Sequelize lifecycle.
 		 * The `models/index` file will call this method automatically.
 		 */
-		static associate(models) {
+		static associate({Event, StageEvent, SetTime}) {
 			// define association here
+            Stage.belongsToMany(Event, {
+                through: StageEvent,
+                foreignKey:'stage_id',
+                as: 'events'
+
+            })
+            Stage.hasMany(SetTime, {
+                foreignKey:'stage_id',
+                as:'set_times'
+            })
 		}
 	}
 	Stage.init(
